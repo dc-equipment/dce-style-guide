2f9413c8ada01ba99d166bcd8f47923773ee91c9
Myron <myron@dce.co.nz>
2021-09-03 14:31:16 +1200

    Commit of Content/Resources/PageLayouts/Appendix.flpgl,Content/Resources/Templates/Appendix.htm,Content/Resources/Templates/Back_Cover.htm,Content/Resources/Graphics/Icons/caret-right.svg,Content/Resources/Fonts/CombiNumerals_Pro_Bold.otf,Content/Resources/Fonts/CrystalItalic-R6A.otf,Content/Resources/Graphics/dc_eq.svg,Content/Resources/Stylesheets/Default.css,Content/Resources/Graphics/ffe_1.svg,Content/Resources/Snippets/Footer.flsnp,Content/Resources/Templates/Front_Cover.htm,Content/Resources/Stylesheets/GeneratedClasses.css,Content/Resources/PageLayouts/Guide_Appendix.flpgl,Content/Resources/PageLayouts/Guide_Back.flpgl,Content/Resources/PageLayouts/Guide_Chapter.flpgl,Content/Resources/PageLayouts/Guide_Content.flpgl,Content/Resources/PageLayouts/Guide_Content_2Col.flpgl,Content/Resources/PageLayouts/Guide_Front.flpgl,Content/Resources/PageLayouts/Guide_TOC.flpgl,Content/Resources/Graphics/i_Safety_emergencyKit.pdf,Content/Resources/Graphics/i_Safety_emergencyKit.svg,Content/Resources/Graphics/i_Safety_Fire.pdf,Content/Resources/Graphics/i_Safety_Fire.svg,Content/Resources/Graphics/i_Safety_flyingDebris.pdf,Content/Resources/Graphics/i_Safety_flyingDebris.svg,Content/Resources/Graphics/i_Safety_handholdsSteps.pdf,Content/Resources/Graphics/i_Safety_handholdsSteps.svg,Content/Resources/Graphics/i_Safety_highPressureFluids.pdf,Content/Resources/Graphics/i_Safety_highPressureFluids.svg,Content/Resources/Graphics/i_Safety_Inspect.pdf,Content/Resources/Graphics/i_Safety_Inspect.svg,Content/Resources/Graphics/i_Safety_liftingEquipment.pdf,Content/Resources/Graphics/i_Safety_liftingEquipment.svg,Content/Resources/Graphics/i_Safety_Modifications.pdf,Content/Resources/Graphics/i_Safety_Modifications.svg,Content/Resources/Graphics/i_Safety_movingParts.pdf,Content/Resources/Graphics/i_Safety_movingParts.svg,Content/Resources/Graphics/i_Safety_Overhead.pdf,Content/Resources/Graphics/i_Safety_Overhead.svg,Content/Resources/Graphics/i_Safety_PPE.pdf,Content/Resources/Graphics/i_Safety_PPE.svg,Content/Resources/Graphics/i_Safety_Read.pdf,Content/Resources/Graphics/i_Safety_Read.svg,Content/Resources/Graphics/i_Safety_Welding.pdf,Content/Resources/Graphics/i_Safety_Welding.svg,Content/Resources/Templates/Index.htm,Content/Resources/Graphics/Icons/ISO_7010_W001.svg,Content/Resources/Graphics/Icons/ISO_ALERT.svg,Content/Resources/MasterPages/MasterPage.flmsp,Content/Resources/Graphics/Icons/note.svg,Content/Resources/Stylesheets/PDF.css,Content/Resources/Graphics/pic_watermarkDRAFT.png,Content/Resources/Stylesheets/Print.css,Content/Resources/Graphics/Icons/safety_alert_black.svg,Content/Resources/Graphics/Icons/safety_alert_white.svg,Content/Resources/Fonts/SCDingbats.otf,Content/Resources/Scripts/SkinMods.js,Content/Resources/Graphics/Icons/tip.svg,Content/Resources/Templates/TOC.htm,Content/Resources/Snippets/Watermark.flsnp,Content/Resources/Stylesheets/Web.css

---
+++ Content/Resources/Scripts/SkinMods.js
@@ -0,0 +1,46 @@
+﻿/* ModCap Skin Modifications example */
+/* You can use these script examples in your skin. 
+   a) Open the skin 'Toolbar' tab, and add these examples in the 'Custom javascript' secion.
+   b) Load this js file. */
+
+/* Example 1 - How to add a title to the header area */
+/* This adds a title to the header area that will display the document title, which is set using the skin 'Caption' on the 'General' tab */
+
+$(document).ready(function(){
+	/* (1) This adds a title (div#header-title) to the header area after the logo (#header>a) for the normal desktop layout */
+		
+	$("#header>a").after('<div id="header-title"></div>');
+	$("#header-title").text(document.title);
+		
+	/* (2) This next part adds a title (div#responsive-header-title) to the header area after the logo (#responsiveHeader>a) for the responsive layout. */
+		
+	$("#responsiveHeader>a").after('<div id="responsive-header-title"></div>');
+	$("#responsive-header-title").text(document.title);
+		
+});
+/* End Example 1 */
+
+
+/* Example 2 - How to add a link to a skin stylesheet */
+/* This adds a link to the skin.css file - Content/Resources/Styleheets/skin.css */
+$('<link>')
+	.appendTo($('head'))
+	.attr({type: 'text/css', rel: 'stylesheet'})
+	.attr('href', 'Content/Resources/Stylesheets/skin.css');
+
+	/* link also included in lowercase, in target is set to use lowercase filenames */
+$('<link>')
+	.appendTo($('head'))
+	.attr({type: 'text/css', rel: 'stylesheet'})
+	.attr('href', 'content/resources/stylesheets/skin.css');
+/* End Example 2 */
+
+
+/* Example 3 - How to move the search box in responsive layout */
+/* This will move the search box (#search-sidebar) in responsive layout from the menu and into the header area (#responsiveHeader) */
+
+$(document).ready(function(){
+		/* Use appendTo to move #search-sidebar to the #responsiveHeader */
+		$('#search-sidebar').appendTo('#responsiveHeader');
+});
+/* End Example 3 */
